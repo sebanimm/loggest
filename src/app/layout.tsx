dee85e334bc37ffc1ec479fce19a0e555bd37757
sebanimm <gks32114221@gmail.com>
2024-06-24 02:49:09 +0900

    style: eslint 규칙대로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import { ThemeProvider } from "@/components/themeProvider";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import google from "next-auth/providers/google";
+import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
