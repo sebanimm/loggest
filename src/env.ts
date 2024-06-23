7d1cce0f910187d77e82a8efc6c0ee8c6f80b87e
sebanimm <gks32114221@gmail.com>
2024-06-23 21:45:18 +0900

    feat: env에 최소길이 추가

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 const ENV_SCHEMA = z.object({
-  DATABASE_URL: z.string().url(),
-  AUTH_SECRET: z.string(),
-  AUTH_GITHUB_ID: z.string(),
-  AUTH_GITHUB_SECRET: z.string(),
-  AUTH_GOOGLE_ID: z.string(),
-  AUTH_GOOGLE_SECRET: z.string(),
-  SERVER_BASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url().min(1),
+  AUTH_SECRET: z.string().min(1),
+  AUTH_GITHUB_ID: z.string().min(1),
+  AUTH_GITHUB_SECRET: z.string().min(1),
+  AUTH_GOOGLE_ID: z.string().min(1),
+  AUTH_GOOGLE_SECRET: z.string().min(1),
+  SERVER_BASE_URL: z.string().url().min(1),
 });
 
 const env = ENV_SCHEMA.parse(process.env);
