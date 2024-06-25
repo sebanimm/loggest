87e51388f0f556f70b732d8c5f5dbd11ae5b7e5c
sebanimm <gks32114221@gmail.com>
2024-06-25 12:06:30 +0900

    feat: 헤더에 인증에 따라 글 작성 버튼을 보여주도록 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,23 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Logo from "@/assets/logo.svg";
+import { auth } from "@/auth";
 import AuthUser from "@/components/authUser";
 import ThemeToggle from "@/components/common/ThemeToggle";
 import Search from "@/components/search";
 import { Button } from "@/components/ui/button";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
-    <header className="sticky top-0 z-50">
+    <header className="sticky top-0 z-50 bg-background">
       <div className="container flex h-14 items-center">
         <Link href="/">
           <Image src={Logo} alt="loggest" className="hidden sm:block" />
         </Link>
         <div className="flex flex-1 items-center justify-end gap-2">
           <Search />
-          <Button variant="ghost" size="icon" className="w-10">
-            <Plus />
-          </Button>
+          {session?.user && (
+            <Button variant="ghost" size="icon" className="w-10">
+              <Plus />
+            </Button>
+          )}
           <ThemeToggle />
           <AuthUser />
         </div>
