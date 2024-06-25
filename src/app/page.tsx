337593cdf94248b7bbd7e1d68c313814466c9431
sebanimm <gks32114221@gmail.com>
2024-06-25 12:30:31 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PostList from "@/components/common/PostList";
 
 export default function Home() {
   return (
-    <div className="container pt-8">
+    <div className="container py-8">
       <main className="grid grid-cols-3 gap-4 lg:grid-cols-4">
         <PostList />
       </main>
