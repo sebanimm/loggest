735e3a7a1ec053f872fe883eb54692f3f44c40ab
sebanimm <gks32114221@gmail.com>
2024-06-29 20:07:37 +0900

    refactor: 전체 유저 이름 조회를 id 조회로 변경

--- src/app/api/user/userIds/route.ts
+++ src/app/api/user/userIds/route.ts
@@ -2,9 +2,9 @@ import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
   try {
-    const users = await prisma.user.findMany({ select: { name: true } });
-    const usernames = users.map((user) => user.name);
-    return Response.json(usernames);
+    const users = await prisma.user.findMany({ select: { id: true } });
+    const userIds = users.map((user) => user.id);
+    return Response.json(userIds);
   } catch (err) {
     console.log(err);
     return Response.json({ message: "에러발생" });
