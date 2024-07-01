06fedb87d5583267acf2c245fabbb75753f28f8f
sebanimm <gks32114221@gmail.com>
2024-07-01 10:07:52 +0900

    feat: jotai provider 추가

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,16 @@
+import { Provider as JotaiProvider } from "jotai";
 import { SessionProvider } from "next-auth/react";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { MultiProvider } from "@/providers/multiProvider";
 import { ThemeProvider } from "@/providers/themeProvider";
 
-const provider = [TooltipProvider, ThemeProvider, SessionProvider];
+const provider = [
+  TooltipProvider,
+  ThemeProvider,
+  SessionProvider,
+  JotaiProvider,
+];
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <MultiProvider providers={provider}>{children}</MultiProvider>;
